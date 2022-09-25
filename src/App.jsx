895a032a414a351ef79bc02f96dd8987f4314768
ohchad <chad.steele@obviohealth.com>
2022-09-25 18:19:38 -0500

    daisyui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "tailwindcss/tailwind.css"
+
+
+
+
+function App () {
+  return (
+    <div class="container mx-auto m-5 prose" data-theme="light">
+      <h1 >quizzr</h1>
+      <textarea class="textarea-bordered textarea min-w-full" placeholder="Cut'n Paste your document here"></textarea>
+      <a class="btn btn-primary">Go</a>
+    </div >
+  )
+}
+
+export default App;
